5e427c0df6b65616ff6c2848a6390ea4559ba9b0
Udeyx <udeyxx@gmail.com>
2023-07-24 21:54:22 +0800

    fix:修改注册函数

--- DataBase/people.py
+++ DataBase/people.py
@@ -6,7 +6,9 @@ class PeopleTb:
         return len(self.execute(f"select * from people where name = '{name}' and passwd = '{passwd}';")) != 0
 
     def sign_up(self, name, nick, passwd):
-        if self.sign_in(name, passwd):
+        name = "'" + name + "'"
+        exist = self.execute(f'select * from people where name = {name};')
+        if len(exist) > 0:
             return False
         self.execute(
             f"insert into people (name, nick, passwd, sex, birth, fav, ates) values ('{name}', '{nick}', '{passwd}', 0, '', 0, 0);")
